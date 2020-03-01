b3911b6c78e3bd8cc1a01e2d7bb5f8203a07cfc0
maxin <1285590084@qq.com>
2020-03-01 22:10:17 +0800

    feat: 添加定时清除test submit的任务

--- oj/celery_config.py
+++ oj/celery_config.py
@@ -7,15 +7,12 @@ CELERY_TASK_SOFT_TIME_LIMIT = CELERY_TASK_TIME_LIMIT = 180
 CELERYD_MAX_TASKS_PER_CHILD = 20
 CELERY_ACKS_LATE = True
 
-
 CELERY_IMPORTS = (
-     'oj.tasks',
+    'oj.tasks',
 )
-
 
 CELERY_ACCEPT_CONTENT = ["msgpack"]
 CELERY_TASK_SERIALIZER = "msgpack"
-CELERY_TASK_SERIALIZER = 'msgpack'
 
 # 每个result的生命周期
 CELERY_TASK_RESULT_EXPIRES = 100
@@ -28,10 +25,9 @@ CELERYBEAT_SCHEDULE = {
         'task': "oj.tasks.daily_info_count",
         'schedule': crontab(minute=57, hour=23),
     },
-    'daily_task_clean_submission':{
-        'task':'oj.tasks.clean_test_submission',
-        'schedule': crontab(minute=10, hour=18)
+    'daily_task_clean_submission': {
+        'task': 'oj.tasks.clean_test_submission',
+        'schedule': crontab(minute=30, hour=2)
     }
 
 }
-
