2427ac41c2ccb43682921c36f23c03966fc2fddd
ma <1285590084@qq.com>
2020-04-18 15:21:26 +0800

    feat: 增加提交浏览量
    feat: submit task

---
+++ submission/tasks/tasks.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+from django.db.models import F
+from oj.celery import app
+from submission.models import Submission, Likes
+
+
+@app.task
+def increase_submit_view_count(sub_id):
+    Submission.object.filter(pk=sub_id).update(F("view_count")+1)
+    return
